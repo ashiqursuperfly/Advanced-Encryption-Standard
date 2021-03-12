801a67d094e18dfe7b19aa8e1a3aba6cd730f4de
Kh. Ashiqur Rahman <ashiqur.buet16@gmail.com>
2021-03-12 18:43:57 +0600

    add txt file decryption

--- aes/encrypt_file.py
+++ aes/encrypt_file.py
@@ -9,13 +9,23 @@ def append_to_file(content: str, filename: str = "encrypted"):
 
 def encrypt_file(filename: str, key: str):
     file = open(filename, 'r')
+    line = file.read()
 
-    for line in file:
-        while len(line) != 0:
-            to_encrypt = line[0:16]
-            line = line[16:]
-            print(to_encrypt)
+    # while len(line) != 0:
+    #     to_encrypt = line[0:16]
+    #     line = line[16:]
+    #     print(to_encrypt)
+    #     append_to_file(encrypt16(key=key, plaintext=to_encrypt) + '\n')
+
+    to_encrypt = str()
+    for char in line:
+        to_encrypt += char
+        if len(to_encrypt) == 16:
             append_to_file(encrypt16(key=key, plaintext=to_encrypt) + '\n')
+            to_encrypt = ''
+
+    if len(to_encrypt) > 0:
+        append_to_file(encrypt16(key=key, plaintext=to_encrypt.ljust(16)) + '\n')
 
 
 if __name__ == "__main__":
